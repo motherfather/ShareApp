6c9b3f3a9c48dfb155a4131ce0554970b544af96
motherfather <father@mail.com>
2016-11-22 17:59:02 +0900

    Initial commit

---
+++ app/src/main/java/com/bit2016/shareapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.bit2016.shareapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, ShareActivity.class); // 명시적 intent
+        startActivity(intent);
+    }
+}
